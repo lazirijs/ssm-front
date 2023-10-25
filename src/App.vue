479057f1b7dee3645486d75f624d468645b64acf
amar99587 <72984889+amar99587@users.noreply.github.com>
2023-10-25 22:18:27 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <nav-app v-if="user" />
-  <div class="- h-full w-full flex gap-4 overflow-y-auto">
+  <div class="h-full w-full flex gap-4 overflow-y-auto">
     <router-view v-if="user" class="bg-white rounded-v p-4 w-full overflow-y-auto break-words" />
     <div v-else class="bg-white rounded-v p-4 w-full break-words flex justify-center items-center uppercase">Loading...</div>
   </div>
