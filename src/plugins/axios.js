f1fb0e3435bfde1088f1fc213e5dd7537200ada5
amar99587 <72984889+amar99587@users.noreply.github.com>
2023-10-26 07:22:31 +0100

    Update axios.js

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL: "https://ssm-back.onrender.com",
   withCredentials: true,
-  credentials: 'include',
+  //credentials: 'include',
 });
 
 export { api };
