f295d09ed5655858b07928d0cef2f58b2d7dc8ed
amar99587 <trissiti19@gmail.com>
2023-10-26 21:57:25 +0100

    Resolved merge conflicts

--- src/utilities/input.vue
+++ src/utilities/input.vue
@@ -1,20 +1,24 @@
 <template>
     <label v-if="label" class="text-clip">
-        {{label}} 
+        {{ label }}
         <Transition>
             <h6 v-if="subText" :style="`color: ${subTextColor};`" class="inline">{{ subText }}</h6>
         </Transition>
     </label>
-    <div v-if="textarea" class=" rounded-v px-4 py-2 smooth" 
-        :style="`background: #e5e7eb; color: ${invalid ? 'red' : ''}; ${!(loading || readonly) || 'opacity: 70%;cursor: default;'}`">
-        <textarea class="w-full placeholder:text-sm" :readonly="readonly" @:input="emits('update', $event.target.value)" :value="value" :type="type" :placeholder="placeholder || 'enter text here'" :style="`${!(loading || readonly) || 'cursor: default;'}`"></textarea>
+    <div v-if="textarea" class="bg-v-1 rounded-v px-4 py-2 smooth"
+        :style="`color: ${invalid ? 'red' : ''}; ${!(loading || readonly) || 'opacity: 70%;cursor: default;'}`">
+        <textarea class="w-full placeholder:text-sm" :readonly="readonly" @:input="emits('update', $event.target.value)"
+            :value="value" :type="type" :placeholder="placeholder || 'enter text here'"
+            :style="`${!(loading || readonly) || 'cursor: default;'}`"></textarea>
     </div>
     <div v-else v-bind="$attrs"
-        class="w-full h-9 flex items-center rounded-v gap-3 px-4 py-2 border-2 border-dashed smooth" 
-        :style="`background: #e5e7eb; border-color: ${invalid ? 'red' : ''}; ${!(loading || readonly) || 'opacity: 70%;cursor: default;'}`"
-    >
+        class="w-full h-9 flex items-center bg-v-1 rounded-v gap-3 px-4 py-2 border-2 border-dashed smooth"
+        :style="`border-color: ${invalid ? 'red' : ''}; ${!(loading || readonly) || 'opacity: 70%;cursor: default;'}`">
         <icon-app v-if="icon" :icon="loading ? 'svg-spinners:ring-resize' : icon" class="w-7" />
-        <input class="w-full placeholder:text-sm" :readonly="readonly" @:input="emits('update', $event.target.value)" :value="value" :type="type" :placeholder="placeholder || 'enter text here'" :maxlength="maxlength" :minlength="minlength" :style="`${!(loading || readonly) || 'cursor: default;'}`" />
+        <input class="w-full placeholder:text-sm" :readonly="readonly" @:input="emits('update', $event.target.value)"
+            :value="value" :type="type" :placeholder="placeholder || 'enter text here'" :maxlength="maxlength"
+            :minlength="minlength"
+            :style="`${center && '    text-align: center; letter-spacing: .5rem; margin-left: .5rem;'} ${(loading || readonly) && 'cursor: default;'}`" />
         <icon-app v-if="btnIcon" @click="emits('btn')" :icon="btnIcon" class="w-7" />
     </div>
 </template>
@@ -35,6 +39,7 @@ defineProps({
     minlength: String,
     maxlength: String,
     textarea: Boolean,
+    center: Boolean,
 });
 
 const emits = defineEmits(["update", "btn"]);
