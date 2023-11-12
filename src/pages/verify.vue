0ad9445e352de689a75baf3e5bb4301ac846cd23
amar99587 <72984889+amar99587@users.noreply.github.com>
2023-11-12 22:22:30 +0100

    Update verify.vue

--- src/pages/verify.vue
+++ src/pages/verify.vue
@@ -1,6 +1,5 @@
 <template>
-  <div
-    class="bg-white w-full sm:w-6/12 md:w-6/12 lg:w-4/12 flex flex-col items-center text-center gap-4 p-4 rounded-v m-auto mt-0">
+  <div style="height: fit-content !important;" class="bg-white w-full sm:w-6/12 md:w-6/12 lg:w-4/12 flex flex-col items-center text-center gap-4 p-4 rounded-v m-auto mt-0">
     <h3 v-if="OTPfor == 'login' || OTPfor == 'signup'">Verify your Email</h3>
     <h3 v-else>Forget Password</h3>
     <h5 v-if="OTPfor == 'login' || OTPfor == 'signup'" class="w-10/12">
