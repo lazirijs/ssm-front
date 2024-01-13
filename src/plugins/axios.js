cb0b2943674d9d8b11bdbfa9d0b0d061fd2f8998
amar99587 <72984889+amar99587@users.noreply.github.com>
2024-01-13 09:42:39 +0100

    Update axios.js

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://192.168.0.103:8090/", //"https://ssm-back.onrender.com",
+  baseURL: "https://ssm-back.onrender.com",
   withCredentials: true,
 });
 
